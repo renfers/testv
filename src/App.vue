75de24659f956da79f47e9a26a2eb4883b19fd3e
renfers <serge.renfer@gmail.com>
2017-11-01 12:49:18 +0100

    premier commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title>Mon menu</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left>compare_arrows</v-icon>
+          Premier
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+
+</script>
